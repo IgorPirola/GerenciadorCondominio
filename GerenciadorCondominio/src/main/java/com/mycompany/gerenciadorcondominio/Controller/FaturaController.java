a459e24b75e275c46df7198fa76d54ee13ca9760
Igor M. Pirola <90224524+IgorPirola@users.noreply.github.com>
2025-06-02 10:15:27 -0300

    Update FaturaController.java

--- GerenciadorCondominio/src/main/java/com/mycompany/gerenciadorcondominio/Controller/FaturaController.java
+++ GerenciadorCondominio/src/main/java/com/mycompany/gerenciadorcondominio/Controller/FaturaController.java
@@ -14,9 +14,61 @@ import java.sql.SQLException;
  * @author Igor
  */
 public class FaturaController {
-    
-    public boolean insertFatura(Residencia res, double valor, int mes, int ano) throws SQLException{
+    private FaturaDAO fatDAO = new FaturaDAO();
+
+    public boolean insertFatura(int resID, double valor, int mes, int ano){
+        Residencia res = new ResidenciaDAO().bdSelect(resID);
+        
         Faturas fat = new Faturas(res, valor, mes, ano);
         return new FaturasDAO().bdInsert(fat);
+    }
+
+    public void preencherTabela(JTable jtabela){
+        DefaultTableModel dtm = (DefaultTableModel) jtabela.getModel();
+        int tamTabela;
+        int posicaoLinha = 0;
+        
+        try{
+            tamTabela = fatDAO.bdSelectAll().size();
+            
+        }catch (NullPointerException e){
+            tamTabela = 0;
+        }
+        
+        dtm.setRowCount(tamTabela);
+        jtabela.setModel(dtm);
+        
+        if(tamTabela != 0){
+            for(int i=0; i<tamTabela; i++){
+                Faturas fat = fatDAO.bdSelectAll().get(i);
+                jtabela.setValueAt(fat.getId(), posicaoLinha, 0);
+                jtabela.setValueAt(fat.valor(), posicaoLinha, 1);
+                jtabela.setValueAt(fat.mes(), posicaoLinha, 2);
+                jtabela.setValueAt(fat.ano(), posicaoLinha, 3);
+                jtabela.setValueAt(fat.getRes().getId(), posicaoLinha, 4);
+
+                posicaoLinha += 1;
+            }
+        }
+    }
+    
+    public void excluir(JTable jtabela){
+        if(jtabela.getSelectedRow() == -1){
+            JOptionPane.showMessageDialog(null, "Selecione uma fatura da tabela", "Aviso", 0);
+        } else {
+            int resposta = JOptionPane.showConfirmDialog(
+                null,
+                "Tem certeza que deseja excluir a fatura?",
+                "Aviso",
+                JOptionPane.YES_NO_OPTION
+            );
+            
+            if(resposta == JOptionPane.YES_OPTION){
+                fatDAO.bdRemove(Integer.parseInt(jtabela.getValueAt(jtabela.getSelectedRow(), 0).toString()));
+                
+                JOptionPane.showMessageDialog(null, "Fatura excluida com sucesso", "Sucesso", 1);
+                preencherTabela(jtabela);
+            }
+        }
     }
 }
